7eef847ed612da60edd874cc171cb4f4f452b243
adrianbrad <me@adrianbrad.com>
2022-04-18 14:15:53 +0300

    - remove TestMain function

--- psql_container_test.go
+++ psql_container_test.go
@@ -1,40 +1,12 @@
 package psqldocker_test
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/adrianbrad/psqldocker"
 	"github.com/matryer/is"
 	"github.com/ory/dockertest/v3"
 )
-
-func TestMain(m *testing.M) {
-	c, err := psqldocker.NewContainer(
-		"a",
-		"a",
-		"a",
-		psqldocker.WithContainerName("test"),
-		psqldocker.WithSql(
-			"CREATE TABLE users(user_id UUID PRIMARY KEY);",
-		),
-	)
-	if err != nil {
-		fmt.Printf("new container error: %s\n", err)
-		return
-	}
-
-	var ret int
-
-	defer func() {
-		_ = c.Close()
-
-		os.Exit(ret)
-	}()
-
-	m.Run()
-}
 
 func TestNewContainer(t *testing.T) {
 	t.Parallel()
